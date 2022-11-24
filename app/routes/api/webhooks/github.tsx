355b3f3fd52fc8d06acc4ea25ffb6f37435b0d3b
RaccoonRolla <gaspardhenry@gmail.com>
2022-11-24 11:05:59 +0300

    test: some changes to test reminder

--- app/routes/api/webhooks/github.tsx
+++ app/routes/api/webhooks/github.tsx
@@ -1,15 +1,18 @@
 import type { ActionFunction } from "@remix-run/node"; // or "@remix-run/cloudflare"
 import { json } from "@remix-run/node"; // or "@remix-run/cloudflare"
+import { useActionData } from "@remix-run/react";
 import crypto from "crypto";
 
 export const action: ActionFunction = async ({
   request,
 }) => { 
-  console.log(request)
+  console.log(await request.json())
 
-  return {}
+  return json({request: request}, 200)
 };
 
 export default function Github() {
+  const data = useActionData()
+  console.log(data)
   return null
 }
